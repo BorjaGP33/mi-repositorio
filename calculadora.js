c76202e7e125f5753b6b96945427b57b54740cb5
BorjaGP33 <borja1131987@gmail.com>
2023-10-16 16:12:29 +0200

    subida de calculadora

---
+++ calculadora.js
@@ -0,0 +1,32 @@
+function suma (x,y){
+    
+    let resultado = x + y;
+    return resultado;
+}
+
+function resta (x,y){
+
+    let resultado = x -y;
+    return resultado;
+}
+
+function multiplicar (x,y){
+
+    let resultado = x * y;
+    return resultado;
+
+}
+
+// nuevaaaaa!!!
+
+function dividir (x,y){
+
+    let resultado = x / y;
+    return resultado;
+}
+
+
+
+
+
+module.exports = {suma,resta,multiplicar,dividir};
